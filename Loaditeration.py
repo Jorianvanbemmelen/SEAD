11080f2c7507027df03b8f78953fbd748ff5f72f
Jorianvanbemmelen <jorianvanbemmelen@gmail.com>
2023-03-28 22:09:52 +0200

    correct load diagram

--- Loaditeration.py
+++ Loaditeration.py
@@ -1,63 +1,64 @@
 
-
 import numpy as np
 import control
 from math import *
 import matplotlib.pyplot as plt
 
 
-MTOW = 23000  # [kg] Maximum take off weight
-MZFW = 21000  # [kg] Maximum zero fuel weight
-OEW = 13600  # [kg] Operational empty weight
+MTOW = 23000  # [kg]
 MAC = 2.5  # [m]
-x_leadingedge = 11.23 # [m]
-
-# Fuselage group; fuselage, horizontal tailplane, vertical tailplane, nose gear
-
-xcg_f = 13  # [m]
-W_f = 0.248*MTOW  # [kg]
-
-xcg_h = 25  # [m]
-W_h = 0.018*MTOW  # [kg]
-
-xcg_v = 25  # [m]
-W_v = 0.02*MTOW  # [kg]
-
-xcg_ng = 3  # [m]
-W_ng = 0.005*MTOW  # [kg]
-
-xcg_cf = 5  # [m]
-W_cargof = 600 # [kg]
-xcg_cb = 22  # [m]
-W_cargob = 600 # [kg]
-
-seat_pitch = 0.7366  # [m]
-xcg_frontpass = 6  # [m]
-n_rows = 18
-xcg_backpass = xcg_frontpass + (n_rows-1)*seat_pitch  # [m]
-W_2pass = 160  # [kg]
-
-W_fgroup = W_f + W_h + W_v + W_ng + W_cargof + W_cargob + (n_rows)*2*W_2pass
-xcg_fgroup = (xcg_f*W_f + xcg_h*W_h + xcg_v*W_v + xcg_ng*W_ng + xcg_cf*W_cargof + xcg_cb*W_cargob + (xcg_backpass + xcg_frontpass)*n_rows*W_2pass)/(W_f + W_h + W_v + W_ng + W_cargof + W_cargob + n_rows*2*W_2pass)  # [m]
-xcg_fgroup_mac = (xcg_fgroup - x_leadingedge)/MAC  # [MAC]
+x_leadingedge = 11.23  # [m]
 
 # Wing group, wing, main landing gear, propulsion
 
-xcg_w = 14  # [m]
-W_w = 0.149*MTOW  # [kg]
+xcg_w = 14  # [m] Wing
+W_w = 0.149*MTOW   # [kg]
 
-xcg_mg = xcg_w + 3  # [m]
+xcg_mg = xcg_w + 3  # [m] Main landing gear
 W_mg = 0.035*MTOW  #[kg]
 
-xcg_p = xcg_w - 1  # [m]
-W_p = 0.103*MTOW  # [kg]
+xcg_p = xcg_w - 1  # [m] Propulsion including nacelle
+W_p = 0.103*MTOW - 660.1  # [kg]
 
-xcg_fuel = xcg_w
-W_fuel = MTOW - (W_fgroup + W_w + W_mg + W_p)
-
-W_wgroup = W_w + W_mg + W_p + W_fuel
-xcg_wgroup = (xcg_w*W_w + xcg_mg*W_mg + xcg_p*W_p + xcg_fuel*W_fuel)/(W_w + W_mg + W_p + W_fuel) # [m]
+W_wgroup = W_w + W_mg + W_p
+W_p_new = W_p - 0.9*W_wgroup  # 0.1*6601 = 660.1 kg lighter
+xcg_wgroup = (xcg_w*W_w + xcg_mg*W_mg + xcg_p*W_p_new)/(W_w + W_mg + W_p_new) # [m]
 xcg_wgroup_mac = (xcg_wgroup - x_leadingedge)/MAC # [MAC]
+
+# Fuselage group; fuselage, horizontal tailplane, vertical tailplane, nose gear
+
+xcg_cargof = 5  # [-] Cargo front
+xcg_cargob = 22  # [-] Cargo back
+
+W_f = 0.248*MTOW  # [kg] Fuselage
+W_f_new = 1.02*W_f  # [kg] 460 kg increase
+xcg_f = 13  # [m]
+
+W_batt = 1200  # [kg]
+xcg_batt = (xcg_cargob*800 + xcg_cargof*400)/(800 + 400)  # 1200 kg increase
+
+xcg_h = 25  # [m] Horizontal tail plane
+W_h = 0.018*MTOW  # [kg]
+
+xcg_v = 25  # [m] Vertical tailplane
+W_v = 0.02*MTOW  # [kg]
+
+xcg_ng = 3  # [m] Nose gear
+W_ng = 0.005*MTOW  # [kg]
+
+W_fgegroup = W_f + W_h + W_v + W_ng + W_batt
+xcg_fgroup = (xcg_f*W_f_new + xcg_h*W_h + xcg_v*W_v + xcg_ng*W_ng + xcg_batt*W_batt)/(W_f + W_h + W_v + W_ng + W_batt)  # [m]
+xcg_fgroup_mac = (xcg_fgroup - x_leadingedge)/MAC  # [MAC]
+
+# Variable weights
+
+seat_pitch = 0.7366  # [m]
+xcg_frontpass = 6  # [m]
+xcg_backpass = xcg_frontpass + 17*seat_pitch  # [m]
+W_2pass = 160  # [kg]
+
+xcg_cargof = (xcg_cargof - x_leadingedge)/MAC  # [-] Cargo front
+xcg_cargob = (xcg_cargob - x_leadingedge)/MAC  # [-] Cargo back
 
 # x_c.g calculation
 
@@ -65,19 +66,12 @@ xcgOEW = (xcg_w*W_w + xcg_f*W_f + xcg_h*W_h + xcg_v*W_v + xcg_mg*W_mg + xcg_ng*W
 xcgOEWmac = (xcgOEW - x_leadingedge)/MAC  # [MAC]
 W_fixed = W_w + W_f + W_h + W_v + W_mg + W_ng + W_p
 
-xcgMTOW = (xcg_w*W_w + xcg_f*W_f + xcg_h*W_h + xcg_v*W_v + xcg_mg*W_mg + xcg_ng*W_ng + xcg_p*W_p + xcg_fuel*W_fuel + xcg_cf*W_cargof + xcg_cb*W_cargob + (xcg_backpass + xcg_frontpass)*n_rows*W_2pass)/(W_w + W_f + W_h + W_v + W_mg + W_ng + W_p + W_fuel + W_cargof + W_cargob + n_rows*2*W_2pass) # [m]
-xcgMTOWmac = (xcgOEW - x_leadingedge)/MAC  # [MAC]
-W_MTOW = W_w + W_f + W_h + W_v + W_mg + W_ng + W_p + W_cargof + W_cargob + n_rows*2*W_2pass + W_fuel
-
-# For calculations
-# xcg_frontpass = 6 - seat_pitch  # [m]
-# xcg_backpass = xcg_frontpass + (n_rows-1)*seat_pitch + seat_pitch  # [m]
 
 def cargoback(W_fixed, xcgOEW):
     W_old = W_fixed
     xcg_old = xcgOEWmac
-    xcg_cargob = (26 - x_leadingedge)/MAC # [m]
-    xcg_cargof = (4 - x_leadingedge)/MAC # [m]
+    W_cargob = 1000 # [kg]
+    W_cargof = 1000 # [kg]
     xcg_carback = [xcg_old]
     W_carback = [W_old]
     xcg_carb = (W_old*xcg_old + W_cargob*xcg_cargob)/(W_old + W_cargob)
@@ -95,8 +89,8 @@ xcg_carback, W_carback = cargoback(W_fixed, xcgOEW)
 def cargofront(W_fixed, xcgOEW):
     W_old = W_fixed
     xcg_old = xcgOEWmac
-    xcg_cargob = (26 - x_leadingedge)/MAC  # [m]
-    xcg_cargof = (4 - x_leadingedge)/MAC  # [m]
+    W_cargob = 1000  # [N]
+    W_cargof = 1000  # [N]
     xcg_carfront = [xcg_old]
     W_carfront = [W_old]
     xcg_carf = (W_old*xcg_old + W_cargof*xcg_cargof)/(W_old + W_cargof)
@@ -115,10 +109,10 @@ def passforwardwindow(W_carback, xcg_carback, W_2pass, seat_pitch):
     # print(W_carback)
     W_old = W_carback[2]
     xcg_old = xcg_carback[2]
-    xcg_p = [(xcg_frontpass - seat_pitch - x_leadingedge)/MAC]
+    xcg_p = [(xcg_frontpass - x_leadingedge)/MAC]
     xcg_list = [xcg_old]
     W_list = [W_old]
-    for i in range(1, (n_rows + 1)):
+    for i in range(1, 17):
         xcg_p.append(xcg_p[i-1] + seat_pitch/MAC)
         xcg_new = (W_old*xcg_old + W_2pass*xcg_p[i])/(W_old + W_2pass)
         xcg_list.append(xcg_new)
@@ -132,10 +126,10 @@ xcg_passforward, W_passforward = passforwardwindow(W_carback, xcg_carback, W_2pa
 def passbackwindow(W_carback, xcg_carback, W_2pass, seat_pitch):
     W_old = W_carback[2]
     xcg_old = xcg_carback[2]
-    xcg_p = [(xcg_backpass + seat_pitch - x_leadingedge)/MAC]
+    xcg_p = [(xcg_backpass - x_leadingedge)/MAC]
     xcg_list = [xcg_old]
     W_list = [W_old]
-    for i in range(1, (n_rows + 1)):
+    for i in range(1, 17):
         xcg_p.append(xcg_p[i-1] - seat_pitch/MAC)
         xcg_new = (W_old*xcg_old + W_2pass*xcg_p[i])/(W_old + W_2pass)
         xcg_list.append(xcg_new)
@@ -149,10 +143,10 @@ xcg_passback, W_passback = passbackwindow(W_carback, xcg_carback, W_2pass, seat_
 def passforwardaisle(W_passforward, xcg_passforward, W_2pass, seat_pitch):
     W_old = W_passforward[-1]
     xcg_old = xcg_passforward[-1]
-    xcg_p = [(xcg_frontpass - seat_pitch - x_leadingedge)/MAC]
+    xcg_p = [(xcg_frontpass - x_leadingedge)/MAC]
     xcg_list = [xcg_old]
     W_list = [W_old]
-    for i in range(1, (n_rows + 1)):
+    for i in range(1, 17):
         xcg_p.append(xcg_p[i-1] + seat_pitch/MAC)
         xcg_new = (W_old*xcg_old + W_2pass*xcg_p[i])/(W_old + W_2pass)
         xcg_list.append(xcg_new)
@@ -166,10 +160,10 @@ xcg_passforwardaisle, W_passforwardaisle = passforwardaisle(W_passforward, xcg_p
 def passbackaisle(W_passforward, xcg_passforward, W_2pass, seat_pitch):
     W_old = W_passforward[-1]
     xcg_old = xcg_passforward[-1]
-    xcg_p = [(xcg_backpass + seat_pitch - x_leadingedge)/MAC]
+    xcg_p = [(xcg_backpass - x_leadingedge)/MAC]
     xcg_list = [xcg_old]
     W_list = [W_old]
-    for i in range(1, (n_rows + 1)):
+    for i in range(1, 17):
         xcg_p.append(xcg_p[i-1] - seat_pitch/MAC)
         xcg_new = (W_old*xcg_old + W_2pass*xcg_p[i])/(W_old + W_2pass)
         xcg_list.append(xcg_new)
@@ -184,11 +178,12 @@ def fuel(xcg_passbackaisle, W_passbackaisle):
     W_old = W_passbackaisle[-1]
     xcg_old = xcg_passbackaisle[-1]
     xcg_fuel = (xcg_w - x_leadingedge)/MAC # [m]
+    W_fuel = 5000 - 1000 # [kg]
     W_fueltotal = [W_old]
     xcg_fueltotal = [xcg_old]
     xcg_total = (W_passbackaisle[-1]*xcg_passbackaisle[-1] + W_fuel*xcg_fuel)/(W_passbackaisle[-1] + W_fuel)
     W_total = W_passbackaisle[-1] + W_fuel
-    xcg_fueltotal.append(xcg_total)
+    xcg_fueltotal.append(xcg_fuel)
     W_fueltotal.append(W_total)
     return xcg_fueltotal, W_fueltotal
 
